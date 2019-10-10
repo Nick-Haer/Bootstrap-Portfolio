863e5da03a4013ee80e52843a4ddc7628f314ba0
Nick.Haer <nhaer123@gmail.com>
2019-10-10 16:11:49 -0500

    added first route to index

--- controller/htmlRoutes.js
+++ controller/htmlRoutes.js
@@ -1,9 +1,11 @@
+const path = require("path")
+
 module.exports = function(app) {
 
 
 
     app.get("/", function(req, res) {
-        res.send
+        res.send(path.join(__dirname + "/public/index.html"))
     })
 
 }
