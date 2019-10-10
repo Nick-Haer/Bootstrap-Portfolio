a1ab7a3e004a24aa64da661eafc409276e965b05
Nick.Haer <nhaer123@gmail.com>
2019-10-10 16:30:09 -0500

    began serving files through routes in controller

--- controller/htmlRoutes.js
+++ controller/htmlRoutes.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
 
 
     app.get("/", function(req, res) {
-        res.send(path.join(__dirname + "/public/index.html"))
+        res.sendFile(path.join(__dirname, "../public/index.html"))
     })
 
 }
--- server.js
+++ server.js
@@ -4,14 +4,18 @@ const app = express()
 
 let PORT = process.env.PORT || 3000;
 
-const htmlRoutes = require("./controller/htmlRoutes")
 
 
+// app.use("/", htmlRoutes);
+
+app.use(express.static('public'))
 
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
-
+require("./controller/htmlRoutes")(app)
 
 app.listen(PORT, function () {
     console.log(`Listening on port ${PORT}`)
